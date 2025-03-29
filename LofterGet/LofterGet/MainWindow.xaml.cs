52314642c2b0055376514bc4256e396d9ab094a1
SA-Root <a124000@outlook.com>
2025-03-29 22:07:59 +0800

    Update User-Agent; Replace CQ with Channel;

--- LofterGet/LofterGet/MainWindow.xaml.cs
+++ LofterGet/LofterGet/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using System.Collections.Concurrent;
 using System.Net;
+using System.Threading.Channels;
 using System.Xml;
 
 namespace LofterGet;
@@ -12,7 +12,11 @@ namespace LofterGet;
 public sealed partial class MainWindow : Window
 {
     private readonly HttpClient client = new();
-    private readonly ConcurrentQueue<string> queue = new();
+    private readonly Channel<string> channel = Channel.CreateUnbounded<string>(new UnboundedChannelOptions
+    {
+        SingleReader = true,
+        SingleWriter = true,
+    });
     string outFolder;
     int totals = 0;
 
@@ -20,67 +24,53 @@ public sealed partial class MainWindow : Window
     {
         this.InitializeComponent();
         txtReferer.Text = "https://queyangzheng82678.lofter.com/";
-        txtUserAgent.Text = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0";
+        txtUserAgent.Text = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0";
         txtOutputPath.Text = "D:/tmp/M1876";
         outFolder = txtOutputPath.Text;
         client.DefaultRequestHeaders.Add("User-Agent", txtUserAgent.Text);
         client.DefaultRequestHeaders.Add("Referer", txtReferer.Text);
 
-        Task.Run(async () =>
+        Task.Run(LofterHandler);
+    }
+
+    private async Task LofterHandler()
+    {
+        while (true)
         {
-            while (true)
+            var uri = await channel.Reader.ReadAsync();
+            var resp = client.Send(new HttpRequestMessage
             {
-                if (queue.TryDequeue(out var uri))
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(uri),
+            });
+            if (resp.StatusCode == HttpStatusCode.OK)
+            {
+                var file_name = uri[25..].Replace("/", "_");
+                using var f = new FileStream($"{outFolder}/{file_name}",
+                    FileMode.Create, FileAccess.Write);
+                var bin = await resp.Content.ReadAsByteArrayAsync();
+                f.Write(bin);
+                DispatcherQueue.TryEnqueue(() =>
                 {
-                    if (uri != string.Empty)
-                    {
-                        var resp = client.Send(new HttpRequestMessage
-                        {
-                            Method = HttpMethod.Get,
-                            RequestUri = new Uri(uri),
-                        });
-                        if (resp.StatusCode == HttpStatusCode.OK)
-                        {
-                            var file_name = uri[25..].Replace("/", "_");
-                            using var f = new FileStream($"{outFolder}/{file_name}",
-                                FileMode.Create, FileAccess.Write);
-                            var bin = await resp.Content.ReadAsByteArrayAsync();
-                            f.Write(bin);
-                            DispatcherQueue.TryEnqueue(() =>
-                            {
-                                pBar.Value += 100.0 / totals;
-                                txtConsole.Text += $"{resp.StatusCode}: {file_name}\r\n";
-                                svConsole.ScrollTo(0, svConsole.ExtentHeight);
-                            });
-                            Thread.Sleep(500);
-                        }
-                    }
-                    else
-                    {
-                        DispatcherQueue.TryEnqueue(() =>
-                        {
-                            pBar.Value += 100.0 / totals;
-                        });
-                    }
-                }
-                else
-                {
-                    Thread.Sleep(100);
-                }
+                    pBar.Value += 100.0 / totals;
+                    txtConsole.Text += $"{resp.StatusCode}: {file_name}\r\n";
+                    svConsole.ScrollTo(0, svConsole.ExtentHeight);
+                });
+                Thread.Sleep(500);
             }
-        });
+        }
     }
 
     private void myButton_Click(object sender, RoutedEventArgs e)
     {
-        var uris = txtSources.Text.Split('\r');
+        var uris = txtSources.Text.Split('\r', StringSplitOptions.RemoveEmptyEntries);
         if (uris.Length > 0)
         {
             pBar.Value = 0;
             totals = uris.Length;
             foreach (var uri in uris)
             {
-                queue.Enqueue(uri);
+                channel.Writer.WriteAsync(uri);
             }
             txtSources.Text = string.Empty;
         }
@@ -102,5 +92,10 @@ public sealed partial class MainWindow : Window
             xr.ReadToFollowing("arm64binary");
             txtOneDrive.Text += xr["url"];
         }
+    }
+
+    private void Window_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        txtSources.Focus(FocusState.Programmatic);
     }
 }
