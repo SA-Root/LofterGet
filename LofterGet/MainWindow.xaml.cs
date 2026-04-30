27c7cd07d32541e918df67a2403cea1461514026
SA-Root <a124000@outlook.com>
2026-04-30 23:11:37 +0800

    Remove publish profiles;

--- LofterGet/MainWindow.xaml.cs
+++ LofterGet/MainWindow.xaml.cs
@@ -113,7 +113,6 @@ public sealed partial class MainWindow : Window
                 {
                     pBar.Value += 100.0 / totals;
                     txtConsole.Text += $"{resp.StatusCode}: {file_name}\r\n";
-                    svConsole.ScrollTo(0, svConsole.ExtentHeight);
                 });
                 await Task.Delay(500);
             }
